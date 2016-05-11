820340568d365be1d05bd1b6af07a7fb2a62beef
wacky515@gmail.com <firebomberd7>
2016-05-11 09:06:04 +0900

    2016/05/11 3rd

--- trim.py
+++ trim.py
@@ -20,7 +20,6 @@ import cv2
 import cv2.cv as cv
 import os
 import tplmatching as tpm
-import getch
 
 import sys
 # sysモジュール リロード
@@ -38,21 +37,17 @@ class Trim(tpm.GetImage):
     def start_trim(self, image):
         u""" トリミング 開始 """
         gim = tpm.GetImage(image)
-        gim.display("Original image")
-#         key = ord(getch.getch())
-        event = 0
-        coor_x = 0
-        coor_y = 0
-        if event == cv2.EVENT_LBUTTONDOWN:
-            # 古い矩形を消去（2回目以降の描画用） 一旦キル！！！
-            # cv2.imshow("Original image", image)
-            # while(event > 0):
-            print "Selected positon:" + str(coor_x) + ", " + str(coor_y)
-            if(key == 3):  # key 3:"Ctrl + c"
-                print("Quit")
-                # break
+        window_name = "Original image"
+        gim.display(window_name)
+        cv2.setMousecallback(window_name, self.mouse_event, 0)
 
-        tpm.termination(0, 0)
+    def mouse_event(self, event, coor_x, coor_y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            coor_x, coor_y = 0
+            print "Selected positon:" + str(coor_x) + ", " + str(coor_y)
+            if cv2.waitkey(33) > 0:
+                print("Quit")
+                tpm.termination(0, 0)
 
         # if event == cv2.EVENT_LBUTTONDOWN:
 # 左クリック押下 画像再描画（いらない表示を消す＝文字のない画像を受け取る！！）
