139447896fead4fa50dd1bb13e7d8eb382847513
wacky515@gmail.com <firebomberd7>
2016-05-24 16:32:14 +0900

    インスタンス変数の宣言場所ミス 修正

--- trim.py
+++ trim.py
@@ -31,96 +31,98 @@ sys.setdefaultencoding("utf-8")
 
 class Trim:
     u""" トリミング クラス """
-    # 矩形描画用 クラス変数郡
-    start_x = 0
-    start_y = 0
-    end_x = 0
-    end_y = 0
-    length_x = 0
-    length_y = 0
-    coor_x = 0
-    coor_y = 0
-
-    # テキスト描画用 クラス変数群
-    text_offset = 10
-    baseline = 0
-    baseline_upper = 0
-    text1 = "Select area: Drag center"
-    text2 = "Quit: Long press \"Esc\" key"
-    text3 = "Save: Long press \"s\" key"
-
-    # その他 クラス変数郡
-    save_flg = False
-    window_name = "Original image"
 
     def __init__(self, img, save_name):
+        # 画像読込み用 インスタンス変数郡
         self.img = img
         self.save_name = save_name
         self.image = cv2.imread(self.img, 1)
         self.size = self.image.shape
 
+        # 矩形描画用 インスタンス変数郡
+        self.start_x = 0
+        self.start_y = 0
+        self.end_x = 0
+        self.end_y = 0
+        self.length_x = 0
+        self.length_y = 0
+        self.coor_x = 0
+        self.coor_y = 0
+
+        # テキスト描画用 インスタンス変数郡
+        self.text_offset = 10
+        self.baseline = 0
+        self.baseline_upper = 0
+        self.text1 = "Select area: Drag center"
+        self.text2 = "Quit: Long press \"Esc\" key"
+        self.text3 = "Save: Long press \"s\" key"
+
+        # その他 インスタンス変数郡
+        self.save_flg = False
+        self.window_name = "Original image"
+
     def start_trim(self):
         u""" トリミング 開始 """
-        cv2.namedWindow(Trim.window_name, cv2.WINDOW_AUTOSIZE)
-        cv2.setMouseCallback(Trim.window_name, self.mouse_event)
+        cv2.namedWindow(self.window_name, cv2.WINDOW_AUTOSIZE)
+        cv2.setMouseCallback(self.window_name, self.mouse_event)
 
         # 読込み画像の大きさから文字描画のベースライン 取得
-        Trim.baseline = self.size[0] - Trim.text_offset
+        self.baseline = self.size[0] - self.text_offset
 
         # 操作方法説明文 表示
-        text_height = self.write_text(Trim.text1, (1, Trim.baseline))
-        Trim.baseline_upper = text_height[1] + Trim.text_offset / 2
-        self.write_text(Trim.text2, (1, Trim.baseline_upper))
+        text_height = self.write_text(self.text1, (1, self.baseline))
+        self.baseline_upper = text_height[1] + self.text_offset / 2
+        self.write_text(self.text2, (1, self.baseline_upper))
 
-        cv2.imshow(Trim.window_name, self.image)
+        cv2.imshow(self.window_name, self.image)
 
         # テスト出力
-        print "Trim pos: (" + str(Trim.start_x) + ", "\
-            + str(Trim.start_y) + "), ("\
-            + str(Trim.end_x) + ", "\
-            + str(Trim.end_y) + ")"
+        print "Trim pos: (" + str(self.start_x) + ", "\
+            + str(self.start_y) + "), ("\
+            + str(self.end_x) + ", "\
+            + str(self.end_y) + ")"
 
         tpm.termination(0, 0)
 
     def mouse_event(self, event, coor_x, coor_y, flags, param):
         u""" マウスイベント 取得 """
-        Trim.coor_x = coor_x
-        Trim.coor_y = coor_y
+        self.coor_x = coor_x
+        self.coor_y = coor_y
 
         if event == cv2.EVENT_LBUTTONDOWN:
-            Trim.start_x = Trim.start_y = Trim.end_x = Trim.end_y = 0
+            self.start_x = self.start_y = self.end_x = self.end_y = 0
             """ 2回目以降に古い描画を消去するため
             左クリック押下毎に対象画像を読込み """
             self.image = cv2.imread(self.img, 1)
 
-            Trim.start_x, Trim.start_y = Trim.coor_x, Trim.coor_y
-            Trim.save_flg is False
+            self.start_x, self.start_y = self.coor_x, self.coor_y
+            self.save_flg is False
             # テスト出力
-            print "Start: " + str(Trim.start_x) + ", " + str(Trim.start_y)
+            print "Start: " + str(self.start_x) + ", " + str(self.start_y)
 
         elif event == cv2.EVENT_LBUTTONUP:
-            Trim.end_x, Trim.end_y = Trim.coor_x, Trim.coor_y
+            self.end_x, self.end_y = self.coor_x, self.coor_y
             self.image = cv2.imread(self.img, 1)
 
             # 操作方法説明文 表示
-            text_height = self.write_text(Trim.text1, (1, Trim.baseline))
-            self.write_text(Trim.text2, (1, Trim.baseline_upper))
+            text_height = self.write_text(self.text1, (1, self.baseline))
+            self.write_text(self.text2, (1, self.baseline_upper))
             baseline_mid =\
-                Trim.baseline - (text_height[1] + Trim.text_offset)
-            self.write_text(Trim.text3, (1, baseline_mid))
+                self.baseline - (text_height[1] + self.text_offset)
+            self.write_text(self.text3, (1, baseline_mid))
 
             # 矩形 描画
             self.draw_rectangle()
 
-            cv2.imshow(Trim.window_name, self.image)
-            Trim.save_flg is True
+            cv2.imshow(self.window_name, self.image)
+            self.save_flg is True
 
             # テスト出力
-            print "End: " + str(Trim.end_x) + ", " + str(Trim.end_y)
-            print "Trim area: (" + str(Trim.start_x) + ", "\
-                + str(Trim.start_y) + "), ("\
-                + str(Trim.end_x) + ", "\
-                + str(Trim.end_y) + ")"
+            print "End: " + str(self.end_x) + ", " + str(self.end_y)
+            print "Trim area: (" + str(self.start_x) + ", "\
+                + str(self.start_y) + "), ("\
+                + str(self.end_x) + ", "\
+                + str(self.end_y) + ")"
 
         elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
             """ 古い矩形描画を消去するため
@@ -130,21 +132,21 @@ class Trim:
             # 矩形 描画
             self.draw_rectangle()
 
-            cv2.imshow(Trim.window_name, self.image)
+            cv2.imshow(self.window_name, self.image)
 
             # テスト出力
-            print "Select: " + str(Trim.coor_x) + ", " + str(Trim.coor_y)
+            print "Select: " + str(self.coor_x) + ", " + str(self.coor_y)
 
-        # 2016/05/24 ここまで！！！ s押下したら終了する！！！
-        if cv2.waitKey(33) == ord("s") and Trim.save_flg is True:
+        # 2016/05/24 ここまで！！！ "s" 押下したら終了する！！！
+        if cv2.waitKey(33) == ord("s") and self.save_flg is True:
             height = 0
             width = 0
             trim_image =\
-                self.image[height: Trim.start_y + Trim.length_y,
-                        width: Trim.start_x + Trim.length_x]
-            cv2.imwrite(Trim.save_name, trim_image)
+                self.image[height: self.start_y + self.length_y,
+                        width: self.start_x + self.length_x]
+            cv2.imwrite(self.save_name, trim_image)
 
-        return Trim.start_x, Trim.start_y, Trim.end_x, Trim.end_y
+        return self.start_x, self.start_y, self.end_x, self.end_y
 
     def write_text(self, text, origin,
             cpt=cv2.putText,
@@ -163,10 +165,10 @@ class Trim:
     def draw_rectangle(self):
         u""" 矩形 描画 """
         cra = cv2.rectangle
-        Trim.length_x = 2 * Trim.start_x - Trim.coor_x
-        Trim.length_y = 2 * Trim.start_y - Trim.coor_y
-        start_point = (Trim.length_x, Trim.length_y)
-        end_point = (Trim.coor_x, Trim.coor_y)
+        self.length_x = 2 * self.start_x - self.coor_x
+        self.length_y = 2 * self.start_y - self.coor_y
+        start_point = (self.length_x, self.length_y)
+        end_point = (self.coor_x, self.coor_y)
         color_out = (0, 0, 31)
         color_in = (0, 127, 255)
         thickness_out = 2
