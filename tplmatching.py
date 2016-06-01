db4f3875199fa865be849d8c5edb0e973767c7eb
wacky515@gmail.com <firebomberd7>
2016-06-02 08:47:27 +0900

    コードの小整形

--- tplmatching.py
+++ tplmatching.py
@@ -16,18 +16,18 @@ u""" テンプレートマッチングによる画像処理 """
 
 # TODO: Python3系 対応！！！
 # TODO: 文字列の埋込を % 形式から format 形式に変更
-# TODO: "print" -> "print()" に変更
-# TODO: Unicode文字リテラルを "u"body"" -> ""body"" に変更
+# DONE: "print" -> "print()" に変更
+# TODO: Unicode文字リテラルを " u"body" " -> " "body" " に変更
 
 # モジュール インポート# {{{
 import numpy as np
 import os
-import glob
+# import glob
 import time
 # import unittest
 
 import cv2
-import cv2.cv as cv
+# import cv2.cv as cv
 
 import trim as tm
 
@@ -44,12 +44,14 @@ sys.setdefaultencoding("utf-8")
 # }}}
 
 
-def termination(cap_name=0, wait_time=33):
+def terminate(cap_name=0, wait_time=33):
     u""" 出力画像 終了処理 """# {{{
+    # cap_name: 0: 静止画 1: 動画
     cv2.waitKey(wait_time)
     if cap_name != 0:
         cap_name.release
     cv2.destroyAllWindows()
+    print("Terminated...")
     sys.exit()
 # }}}
 
@@ -59,41 +61,40 @@ class GetImage:
     def __init__(self, image):
         self.image = image
 
-    def get_image(self, conv_type=1):
+    def get_image(self, conversion=1):
         u""" 画像・動画 読込み """
         try:
-            image = cv2.imread(self.image, conv_type)
+            image = cv2.imread(self.image, conversion)
             return image
-            # 画像取得 エラー処理
+        # 画像取得 エラー処理
         except:
-            print "Image data is not found..."
+            print ("Image data is not found...")
             return False
 
     def display(self, window_name, image, _type=1):
-        u""" 画像・動画 画面出力
-        _type: 0: 静止画 1: 動画 切換え"""
-        # 静止画無しの処理
-        # "is None" にした 動作確認！！！
+        u""" 画像・動画 画面出力 """
+        # _type: 0: 静止画 1: 動画 切換え
+        # 静止画無し判定時 処理 ← "is None" にした 動作確認！！！
         if image is None and _type == 0:
-            print "Getting image..."
+            print ("Getting image...")
             image = self.get_image()
-        print "Display %s..." % window_name
+        print ("Display {}s...".format(window_name))
         cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
         cv2.imshow(window_name, image)
-        print u"画像の大きさを取得する処理を実装！！！"
+        print (u"画像の大きさを取得する処理を実装！！！")
         if _type == 0:
             # 静止画の出力保持処理
-            termination(0, 0)
+            terminate(0, 0)
 
 
 class ConvertImage(GetImage):
-    u""" 画像・動画 変換クラス """
+    u""" 画像・動画 変換クラス """  # {{{
     def __init__(self):
         pass
 
     def grayscale(self, image):
         u""" グレースケール 変換処理 """
-        print "Convert grayscale..."
+        print ("Convert grayscale...")
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return gray
 
@@ -125,7 +126,7 @@ class ConvertImage(GetImage):
         # }}}
         subtract = 4
         # 適応的二値化 変換処理
-        print "Convert adaptive threashold..."
+        print ("Convert adaptive threashold...")
         cat = cv2.adaptiveThreshold
         adpth = cat(gray, max_thresh, algo, thresh_type, calc_area, subtract)
         return adpth
@@ -140,29 +141,29 @@ class ConvertImage(GetImage):
         sigma_color = 12
         # 座標空間におけるフィルタシグマ    大きくなるとより遠くの画素同士が影響する
         sigma_metric = 3
-        print "Bilateral filtering..."
+        print ("Bilateral filtering...")
         cvf = cv2.bilateralFilter
         blr = cvf(gray, calc_area, sigma_color, sigma_metric)
         return blr
 
-    def discriminant_analysis(self, image):
+    def discriminantanalyse(self, image):
         u""" 判別分析法 処理 """
         image = self.bilateral_filter(image)
         std_thresh = 40
         max_thresh = 255
         method = cv2.THRESH_BINARY + cv2.THRESH_OTSU
-        print "Discriminant analysing..."
+        print ("Discriminant analysing...")
         cth = cv2.threshold
         ret, dcta = cth(image, std_thresh, max_thresh, method)
         return dcta
 
-    def binarization(self, image):
+    def binarize(self, image):
         u""" 二値化 処理 """
         image = self.bilateral_filter(image)
         std_thresh = 70
         max_thresh = 255
         method = cv2.THRESH_BINARY_INV
-        print "Binarizing..."
+        print ("Binarizing...")
         cth = cv2.threshold
         ret, binz = cth(image, std_thresh, max_thresh, method)
         return binz
@@ -176,9 +177,10 @@ class ConvertImage(GetImage):
         alpha = 0
         beta = 1
         algo = cv2.NORM_MINMAX
-        print "Normalizing..."
+        print ("Normalizing...")
         norm = cv2.normalize(image, alpha, beta, algo)
         return norm
+# }}}
 
 
 class Tplmatching:
@@ -186,7 +188,7 @@ class Tplmatching:
     def __init__(self):
         pass
 
-    def matching(self, image, tpl):
+    def match(self, image, tpl):
         u""" テンプレートマッチング 処理 """
         # 類似判定アルゴリズム 解説# {{{
         # CV_TM_SQDIFF    :輝度値の差の２乗の合計     小さいほど類似
@@ -215,11 +217,11 @@ class ImageProcessing:
         # カメラキャプチャ時のイニシャライズ ディレイ処理
         time.sleep(0.1)
         if not self.cap.isOpened():
-            print "Can not connect camera"
-            termination()
+            print ("Can not connect camera...")
+            terminate()
             # トラックバー 定義(できない)！！！# {{{
             #        bar_name = "Max threshold"
-            #        print max_thresh
+            #        print (max_thresh)
             # # トラックバー 生成
             #        def set_parameter(value):
             #            max_thresh = cv2.getTrackbarPos(bar_name, window_name)
@@ -232,7 +234,6 @@ class ImageProcessing:
 
     def run(self, name, search, extension=".png", dir_master="MasterImage"):
         u""" 動画取得 処理（メインルーチン） """  # {{{
-        # マスター画像 検索
         print ("\r\n-------------------------------------------------")
         print ("Start template matching")
         print ("-------------------------------------------------\r\n")
@@ -243,9 +244,9 @@ class ImageProcessing:
 
         # マスター画像 検索
         sda = sd.SaveData(search, path_master)
-        master, match_flg = sda.get_name_max(extension)
+        master, match_flag = sda.get_name_max(extension)
         print ("\t*** Return search master mode ***\r\n")
-        if match_flg is False:
+        if match_flag is False:
             print ("No match master")
             print ("Go get master mode(no match master case)")
             self.get_master(search, extension, path_master)
@@ -259,17 +260,17 @@ class ImageProcessing:
         count = 0
         while True:
             if count < 1:
-                print "Initial delay"
+                print ("Initial delay")
                 time.sleep(.1)
-            get_flg, frame = self.cap.read()
+            get_flag, frame = self.cap.read()
 
-            if self.check_get_flag(get_flg) is False:
+            if self.check_get_flag(get_flag) is False:
                 break
             if self.check_get_frame(frame) is False:
                 continue
 
             cv2.imshow(name, frame)
-            print "Capture is running..."
+            print ("Capture is running...")
             count += 1
         # !!!: 以上までをclassにしたいが"while"内の"frame"を
         # "while"外に出せないので断念！！！
@@ -279,19 +280,19 @@ class ImageProcessing:
             # 動画 変換・画像処理（まとめる）！！！
             adpth = self.ci.adaptive_threashold(frame)
             self.ci.display("Adaptive threashold", adpth, 1)
-            dcta = self.ci.discriminant_analysis(frame)
+            dcta = self.ci.discriminantanalyse(frame)
             self.ci.display("Discriminant analysis", dcta, 1)
-            binz = self.ci.binarization(frame)
+            binz = self.ci.binarize(frame)
             self.ci.display("Bilateral filter", binz, 1)
 
             # テンプレートマッチング 処理
-            self.tm.matching(frame, master)
-            # print max_value
+            # self.tm.match(frame, master)
+            # print (max_value)
 
             # 仮の終了処理！！！
             if cv2.waitKey(33) > 0:
                 break
-                # termination(cap)
+                # terminate(cap)
 
     def get_master(self, search, extension, path):
         u""" マスター画像 読込み """
@@ -301,7 +302,7 @@ class ImageProcessing:
         text2 = "Quit: Long press \"q\" key"
         text3 = "Trim mode: Long press \"t\" key"
 
-        print "Master image name: " + str(search)
+        print ("Master image name: " + str(search))
 
         self.init_get_camera_image(name)
 
@@ -310,31 +311,32 @@ class ImageProcessing:
             if count < 1:
                 print ("Initial delay")
                 time.sleep(.1)
-            get_flg, frame = self.cap.read()
-            draw_get_flg, draw_frame = self.cap.read()
+            get_flag, frame = self.cap.read()
+            draw_get_flag, draw_frame = self.cap.read()
 
-            if self.check_get_flag(get_flg) is False:
+            if self.check_get_flag(get_flag) is False:
                 break
             if self.check_get_frame(frame) is False:
                 continue
 
-            # 操作方法説明文 表示
+            # 操作方法説明文 表示位置 取得
             text_offset = 10
             baseline = frame.shape[0] - text_offset
             origin = 1, baseline
 
+            # 操作方法説明文 表示
             trim = tm.Trim(draw_frame, search, extension, path, 1)
             text_height = trim.write_text(text2, origin)
             trim.write_text(text3,\
                     (origin[0], origin[1] - text_offset - text_height[1]))
 
             cv2.imshow(name, draw_frame)
-            print "Master captcha"
+            print ("Master captcha")
             count += 1
 
             # "t"キー押下 静止画撮影処理
             if cv2.waitKey(33) == ord("t"):
-                img = "master_source%s" % extension
+                img = "master_source{}s".format(extension)
                 cv2.imwrite(img, frame)
                 trim = tm.Trim(img, search, extension, path)
                 trim.trim()
@@ -344,25 +346,17 @@ class ImageProcessing:
                 print ("\t*** End get master mode ***")
                 break
 
-    # TODO: 重複している 使用しない 将来的に削除！！！
-    def check_is_opened(self, isOpened):
-        u""" 動画取得 エラー処理 """  # {{{
-        if not isOpened:
-            print 'Can not open camera'
-            sys.exit()
-            # }}}
-
     def check_get_flag(self, flag):
         u""" 動画取得ミス時 スキップ処理 """  # {{{
         if flag is False:
-            print "Can not get end flag"
+            print ("Can not get end flag")
             return False
             # }}}
 
     def check_get_frame(self, frame):
         u""" ループ 終了処理 """  # {{{
         if frame is None:
-            print "Can not get video frame"
+            print ("Can not get video frame")
             return False
             # }}}
 
@@ -387,16 +381,18 @@ def main():
     smpl_pic2 = "D:\\OneDrive\\Biz\\Python\\ImageProcessing\\tpl_2.png"
 # }}}
 
-    #  # マスタ画像の最大枝番 取得# {{{
-    # laitest_data = glob.glob(path + "\\*_*[0-9].png")
-    # print max(laitest_data)# }}}
+    # テンプレートマッチング テスト# {{{
+    cip = ImageProcessing()
+    cip.run("Raw capture", "masterImage")
+    # print ("Movie captcha end...")
+    # }}}
 
 #     # 静止画取得 テスト# {{{
 #     gim = GetImage(smpl_pic)
 #     gim2 = GetImage("tpl_3.png")
 #     # gim.diplay("Tes1", 0, 0)
 #     gim2.display("Tes2", 0, 0)
-#     print "Main loop end..."
+#     print ("Main loop end...")
 # # }}}
 
 # # 動画取得 テスト# {{{
@@ -418,18 +414,12 @@ def main():
 #     image = cv2.imread("tpl_2.png")
 #     ci = ConvertImage()
 #     ci.adaptive_threashold(image, "Adaptive Threashold", 0)
-#     print "Sudah cap"
+#     print ("Sudah cap")
 # # }}}
 
-    # 動画変換 テスト# {{{
-    cip = ImageProcessing()
-    cip.run("Raw capture", "masterImage")
-    # print "Movie captcha end..."
-    # }}}
-
     # # ドキュメントストリング# {{{
-    # print GetImage.__doc__
-    # print help(__name__)
+    # print (GetImage.__doc__)
+    # print (help(__name__))
     # }}}
 
 if __name__ == '__main__':
